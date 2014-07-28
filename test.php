e62c99181c1a4686f16cacaa9849555a5c79ed07
Fouad Tabsh <takethiskitteh@gmail.com>
2014-07-28 14:24:11 -0700

    Add even more hotspots to test in SF

--- test.php
+++ test.php
@@ -30,6 +30,8 @@ $pos1->setName("Cory Hall");
 //Ihouse:
 $pos2 = new HotSpot(37.869525, -122.252152, false);
 $pos2->setName("Ihouse");
+$pos2->setMessage("http://www.windykeep.com/temp/music/burgerAd.mp3");
+$pos2->setType(HotSpot::HOTSPOT_TYPE_AD);
 
 //Shattuck:
 $pos3 = new HotSpot(37.867340, -122.267773, false);
@@ -50,19 +52,19 @@ $pos6->setName("Beverly Hills");
 //Forever 21:
 $pos7 = new HotSpot(37.784957, -122.407769, false);
 $pos7->setName("Forever 21");
-$pos7->setMessage("Shop now at Forever 21!");
+$pos7->setMessage("http://www.windykeep.com/temp/music/Annie.mp3");
 $pos7->setType(HotSpot::HOTSPOT_TYPE_AD);
 
 //Starbucks:
 $pos8 = new HotSpot(37.786527, -122.408205, false);
 $pos8->setName("Starbucks");
-$pos8->setMessage("Start your day with a special cup of coffee from Starbucks!");
+$pos8->setMessage("http://www.windykeep.com/temp/music/Annie.mp3");
 $pos8->setType(HotSpot::HOTSPOT_TYPE_AD);
 
 //Victoria's Secret:
 $pos9 = new HotSpot(37.787686, -122.408439, false);
 $pos9->setName("Victoria's Secret");
-$pos9->setMessage("Get the finest perfumes from Victoria's Secret!");
+$pos9->setMessage("http://www.windykeep.com/temp/music/Annie.mp3");
 $pos9->setType(HotSpot::HOTSPOT_TYPE_AD);
 
 //TRAFFIC near RadioShack
@@ -74,7 +76,7 @@ $pos10->setType(HotSpot::HOTSPOT_TYPE_TRAFFIC);
 //Berkeley Hundea
 $pos11 = new HotSpot(37.861612, -122.267533, false);
 $pos11->setName("Berkeley Hunda");
-$pos11->setMessage("You are listening to a Berkeley Hunda advertisement!");
+$pos11->setMessage("http://www.windykeep.com/temp/music/burgerAd.mp3");
 $pos11->setType(HotSpot::HOTSPOT_TYPE_AD);
 
 //TRAFFIC near intersection with Market st.
@@ -86,7 +88,7 @@ $pos12->setType(HotSpot::HOTSPOT_TYPE_TRAFFIC);
 //Grease Box
 $pos13 = new HotSpot(37.843747, -122.276809, false);
 $pos13->setName("Grease Box");
-$pos13->setMessage("You are listening to a Grease Box advertisement!");
+$pos13->setMessage("http://www.windykeep.com/temp/music/starbucksAd.mp3");
 $pos13->setType(HotSpot::HOTSPOT_TYPE_AD);
 
 //TRAFFIC before intersection
@@ -98,11 +100,54 @@ $pos14->setType(HotSpot::HOTSPOT_TYPE_TRAFFIC);
 //Quarter Pound Giant Burger
 $pos15 = new HotSpot(37.837555, -122.282293, false);
 $pos15->setName("Quarter Pound Giant Burger");
-$pos15->setMessage("You are listening to a Quarter Pound Giant Burger advertisement!");
+$pos15->setMessage("http://www.windykeep.com/temp/music/burgerAd.mp3");
 $pos15->setType(HotSpot::HOTSPOT_TYPE_AD);
 
+//Traffic:
+$pos16 = new HotSpot(37.838520, -122.29417, false);
+$pos16->setName("Traffic");
+$pos16->setMessage("Traffic jam up ahead!");
+$pos16->setType(HotSpot::HOTSPOT_TYPE_TRAFFIC);
+
+//Traffic:
+$pos17 = new HotSpot(37.826365, -122.300893, false);
+$pos17->setName("Traffic");
+$pos17->setMessage("Accident up ahead!");
+$pos17->setType(HotSpot::HOTSPOT_TYPE_TRAFFIC);
+
+//Naval Training Station:
+$pos18 = new HotSpot(37.812002, -122.363205, false);
+$pos18->setName("Naval Training Station");
+$pos18->setMessage("http://www.windykeep.com/temp/music/starbucksAd.mp3");
+$pos18->setType(HotSpot::HOTSPOT_TYPE_AD);
+
+//Burger King:
+$pos19 = new HotSpot(37.775494, -122.413402, false);
+$pos19->setName("Burger King");
+$pos19->setMessage("http://www.windykeep.com/temp/music/burgerAd.mp3");
+$pos19->setType(HotSpot::HOTSPOT_TYPE_AD);
+
+//Walgreens:
+$pos20 = new HotSpot(37.77709, -122.416257, false);
+$pos20->setName("Walgreens");
+$pos20->setMessage("http://www.windykeep.com/temp/music/starbucksAd.mp3");
+$pos20->setType(HotSpot::HOTSPOT_TYPE_AD);
+
+//The Stud Bar:
+$pos21 = new HotSpot(37.772843, -122.409961, false);
+$pos21->setName("The Stud Bar");
+$pos21->setMessage("http://www.windykeep.com/temp/music/starbucksAd.mp3");
+$pos21->setType(HotSpot::HOTSPOT_TYPE_AD);
+
+//Annie's Hotdog:
+$pos22 = new HotSpot(37.775710, -122.418763, false);
+$pos22->setName("Annie's Hotdog");
+$pos22->setMessage("http://www.windykeep.com/temp/music/burgerAd.mp3");
+$pos22->setType(HotSpot::HOTSPOT_TYPE_AD);
+
+
 //$posArray = array($pos1, $pos2, $pos3, $pos4, $pos5, $pos6);
-$posArray = array($pos7, $pos8, $pos9, $pos10, $pos11, $pos12, $pos13, $pos14, $pos15);
+$posArray = array($pos2, $pos7, $pos8, $pos9, $pos10, $pos11, $pos12, $pos13, $pos14, $pos15, $pos16, $pos17, $pos18, $pos19, $pos20, $pos21, $pos22);
 
 $kdtree = kdtree($posArray, 0);
 
