d91d7ec9da72c232c88d331ea9e33ccbe0c9f2d0
Fouad Tabsh <takethiskitteh@gmail.com>
2014-07-31 18:41:03 -0700

    Add functionality for addHotSpot.php to add a hotspot to the database

--- addHotSpot.php
+++ addHotSpot.php
@@ -27,12 +27,28 @@ else if(!isset($_GET["long"]) || !preg_match("/^(\+|-)?[0-9]+(.[0-9]+)?$/", $_GE
 }
 
 //All OK:
-echo "All OK!";
+//echo "All OK!";
 
 $name = $_GET["name"];
-$type = $_GET["type"];
+$type = (int) $_GET["type"];
 $message = $_GET["message"];
-$lat = $_GET["lat"];
-$long = $_GET["long"];
+$lat = (double) $_GET["lat"];
+$long = (double) $_GET["long"];
+
+//Connect to the database:
+//$db = new PDO("mysql:dbname=hotspots;host=localhost","root","");
+$db = mysqli_connect('localhost', 'root', '', 'hotspots');
+
+//Make the query:
+$q = "INSERT INTO hotspots (name, type, message, lat, lng) VALUES (?, ?, ?, ?, ?)";
+
+//Prepare the statement:
+$stmt = mysqli_prepare($db, $q);
+
+//Bind the variables:
+mysqli_stmt_bind_param($stmt, 'sisdd', $name, $type, $message, $lat, $long);
+
+//Execute the query:
+mysqli_stmt_execute($stmt);
 
 ?>
